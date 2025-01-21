bf8f00b8643ded0dd1dc3a56fa94f68f2760db09
Pranav Pise <itspranavpise@gmail.com>
2025-01-21 11:26:02 +0530

    project structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 const Home = () => {
   return (
     <div className='flex min-h-screen items-center justify-center'>
-      <Button>
-        Click me
-      </Button>
+      <Link href='/documents/123'>
+        <span className='text-blue-500 underline'>Click Here</span>
+      </Link>
     </div>
   )
 }
