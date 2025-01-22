4d7cdc6bae319517befd48a40671a7a9929a63cc
Pranav Pise <itspranavpise@gmail.com>
2025-01-22 18:02:19 +0530

    Font size tool (custom extension)

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -19,6 +19,7 @@ import Underline from '@tiptap/extension-underline';
 import Link from '@tiptap/extension-link'
 
 import { useEditorStore } from "@/store/use-editor-store";
+import { FontSizeExtension } from '@/extensions/font-size';
 
 export const Editor = () => {
     const { setEditor } = useEditorStore();
@@ -56,6 +57,7 @@ export const Editor = () => {
         },
         extensions: [
             StarterKit,
+            FontSizeExtension,
             FontFamily,
             Color,
             Highlight.configure({ multicolor: true }),
