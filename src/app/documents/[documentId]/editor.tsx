a83019b99a23d8aded8955a2bddb87a831d8f306
Pranav Pise <itspranavpise@gmail.com>
2025-01-23 10:38:44 +0530

    Line Height Tool (custom extension)

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,25 +1,26 @@
-'use client';
+"use client";
 
-import { useEditor, EditorContent } from '@tiptap/react';
-import { Color } from '@tiptap/extension-color';
-import Highlight from '@tiptap/extension-highlight';
-import StarterKit from '@tiptap/starter-kit';
-import FontFamily from '@tiptap/extension-font-family'
-import TaskItem from '@tiptap/extension-task-item';
-import TaskList from '@tiptap/extension-task-list';
-import Table from '@tiptap/extension-table';
-import TableCell from '@tiptap/extension-table-cell';
-import TableHeader from '@tiptap/extension-table-header';
-import TableRow from '@tiptap/extension-table-row';
-import TextStyle from '@tiptap/extension-text-style';
-import TextAlign from '@tiptap/extension-text-align';
-import Image from '@tiptap/extension-image';
-import ImageResize from 'tiptap-extension-resize-image';
-import Underline from '@tiptap/extension-underline';
-import Link from '@tiptap/extension-link'
+import { useEditor, EditorContent } from "@tiptap/react";
+import { Color } from "@tiptap/extension-color";
+import Highlight from "@tiptap/extension-highlight";
+import StarterKit from "@tiptap/starter-kit";
+import FontFamily from "@tiptap/extension-font-family"
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import TextStyle from "@tiptap/extension-text-style";
+import TextAlign from "@tiptap/extension-text-align";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
+import Underline from "@tiptap/extension-underline";
+import Link from "@tiptap/extension-link"
 
 import { useEditorStore } from "@/store/use-editor-store";
-import { FontSizeExtension } from '@/extensions/font-size';
+import { FontSizeExtension } from "@/extensions/font-size";
+import { LineHeightExtension } from "@/extensions/line-height";
 
 export const Editor = () => {
     const { setEditor } = useEditorStore();
@@ -51,13 +52,17 @@ export const Editor = () => {
         },
         editorProps: {
             attributes: {
-                style: 'padding-left: 56px; padding-right: 56px;',
-                class: 'focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
+                style: "padding-left: 56px; padding-right: 56px;",
+                class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
             },
         },
         extensions: [
             StarterKit,
             FontSizeExtension,
+            LineHeightExtension.configure({
+                types: ["heading", "paragraph"],
+                defaultLineHeight: "normal",
+            }), // No need to configure if you want to use the default values, it works properly even then.
             FontFamily,
             Color,
             Highlight.configure({ multicolor: true }),
@@ -73,7 +78,7 @@ export const Editor = () => {
             TableCell,
             TextStyle,
             TextAlign.configure({
-                types: ['heading', 'paragraph'],
+                types: ["heading", "paragraph"],
             }),
             Image,
             ImageResize,
@@ -103,8 +108,8 @@ export const Editor = () => {
     });
     
     return (
-        <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible'>
-            <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
+        <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+            <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
                 <EditorContent editor={editor} />
             </div>
         </div>
