a025d70d903d5444f860258372e8485ed9600130
Pranav Pise <itspranavpise@gmail.com>
2025-01-23 12:05:24 +0530

    Margin Ruler Component

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -664,7 +664,7 @@ export const Toolbar = () => {
     ];
 
     return (
-        <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
+        <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto print:hidden">
             { sections[0].map((item) => (
                 <ToolbarButton
                     key={item.label}
